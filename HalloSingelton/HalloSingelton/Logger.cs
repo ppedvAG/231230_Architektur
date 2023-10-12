1a2c6d461fd546c71de4c3488a375e47bb8b450a
André <ruland@outlook.de>
2023-10-12 09:56:35 +0200

    Hallo singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,38 @@
+﻿namespace HalloSingelton
+{
+    internal class Logger
+    {
+
+        private static Logger? instance;
+        private static readonly object instanceLock = new object();
+        public static Logger Instance
+        {
+            get
+            {
+                if (instance == null)
+                    lock (instanceLock)
+                    {
+                        if (instance == null)
+                            instance = new Logger();
+                    }
+
+                return instance;
+            }
+        }
+
+        private Logger()
+        {
+            Info("New Logger");
+        }
+
+        public void Info(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now:g}] INFO {message}");
+        }
+
+        public void Error(string message)
+        {
+            Console.WriteLine($"[{DateTime.Now:g}] ERROR {message}");
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,9 @@
+﻿using HalloSingelton;
+
+Console.WriteLine("Hello, World!");
+
+Parallel.For(0, 10, i => Logger.Instance.Info($"GO {i}"));
+
+Logger.Instance.Info("Hallo");
+
+Logger.Instance.Error("PANIK!!!");
