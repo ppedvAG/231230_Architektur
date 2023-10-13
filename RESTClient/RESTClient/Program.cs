05ebd5040975656f5b7141c9223e8bbd2de160ed
André <ruland@outlook.de>
2023-10-13 16:32:07 +0200

    REST Client

---
+++ RESTClient/RESTClient/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RESTClient;
+
+Console.WriteLine("Hello, World!");
+
+
+
+var url = "https://www.googleapis.com/books/v1/volumes?q=dotnet";
+
+
+var http = new HttpClient();
+var json = http.GetStringAsync(url).Result;
+
+//Console.WriteLine(json);
+
+
+var books = System.Text.Json.JsonSerializer.Deserialize<Books>(json);
+
+
+foreach (var book in books.items)
+    Console.WriteLine($"{book.volumeInfo.title}");
+
